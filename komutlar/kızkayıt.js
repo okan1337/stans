17d8a15935174032677247cc7a22c9f2f56cbe65
stans <71333815+stxsn@users.noreply.github.com>
2021-04-01 14:07:07 +0300

    Add files via upload

---
+++ komutlar/kızkayıt.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+
+module.exports = {
+kod: ["k", "kız", "kadın"],
+async run (client, message, args) {
+
+  let kız = ('810576767621529650')
+  let kayıtsız = ('810569332353662987')
+  let yetkili = ('810584481953349662')
+
+
+  if (!yetkili) return
+  if (!kız) return
+  if(!message.member.roles.cache.has(yetkili)) {
+     const hata = new Discord.MessageEmbed()
+     .setAuthor(':(', message.author.avatarURL())
+     .setDescription(`Bu Komutu Kullanabilmek İçin <@&810584481953349662> Rolüne Sahip Olmalısınız`)
+     .setColor('#070707')
+     .setTimestamp()
+     return message.channel.send(hata)
+       }
+       let kisi = message.mentions.members.first()
+     if(!kisi)  {
+       const hata = new Discord.MessageEmbed()
+       .setAuthor(':(', message.author.avatarURL())
+       .setDescription(`Lütfen bir kullanıcı belirtin`)
+       .setColor('#070707')
+       .setTimestamp()
+       return message.channel.send(hata)
+         }
+
+         const embed22 = new Discord.MessageEmbed()
+          .setTitle(`Kayıt Başarılı`)
+          .setThumbnail(message.author.avatarURL({dynamic: true}))
+          .setColor("#070707")
+          message.channel.send(embed22)
+
+
+ kisi.roles.add(kız).then(y => y.roles.remove(kayıtsız))
+}
+}
