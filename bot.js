87fbf273297daa94ae9e00a0f1b91a9d077875e3
stans <71333815+stxsn@users.noreply.github.com>
2021-04-03 23:07:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -89,11 +89,7 @@ var game = [
       client.user.setActivity(game[random]);
       }, 4000);
 
-      client.on("message", async message => {
-          if (message.member.hasPermission('MANAGE_GUILD')) return;
-          let links = message.content.match(/(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/gi);
-          if (!links) return;
-          if (message.deletable) message.delete();
+
 
       })
 client.login('ODA0NzcyNTQxMzEzOTc0Mjgz.YBRM1Q.9F04mAOEmdbOEO1QSPBVoP9uJ8k');
